bd0208dec3d10e29721ecf1be7b0c53756c9b7c3
DunnerD <71628263+DunnerD@users.noreply.github.com>
2021-12-09 20:05:35 +0100

    Add files via upload

---
+++ bubblesort.py
@@ -0,0 +1,39 @@
+# Creating a bubble sort function  
+import numpy as np
+from random import randint
+import time
+
+def bubble_sort(list1):  
+    # Outer loop for traverse the entire list  
+    for i in range(0,len(list1)-1):  
+        for j in range(len(list1)-1):  
+            if(list1[j]>list1[j+1]):  
+                temp = list1[j]  
+                list1[j] = list1[j+1]  
+                list1[j+1] = temp  
+    return list1  
+
+def bubble_sort_tester(n):
+  arr = array_gen(n)
+  startTime = time.time()
+  sorted_arr = bubble_sort(arr)
+  stopTime = time.time()
+  return stopTime - startTime
+
+def array_gen(n):
+  arr = []
+  for i in range(n):
+    arr.append(randint(0,n))
+  return arr
+
+print(bubble_sort_tester(10000))
+
+#   steup_code = f"form_main_import{list}"
+#   if list != "sorted" else ""
+#   stmt = f"{list}({array})"
+# times = repeat(setup = setup_code, stmt=stmt, repeat=3, number=10)
+
+# list1 = np.random.randint(1,200000,20000)
+# print("The unsorted list is: ", list)
+# # Calling the bubble sort function  
+# print("The sorted list is: ", bubble_sort(list){min(times)})  
---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+
+
+#buble as class import
+#merge as class impoert
+#buttnes
+st.title("Sorteringsalgoritmer")
+
+result = st.button("Bubblesort")
+
+st.write(result)
+
+result = st.button("Merge sort")
+
+st.write(result)
