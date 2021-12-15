2a35c9a591ef5e14729fef2d13bf350907bb656b
DunnerD <duncanmjk@gmail.com>
2021-12-16 00:09:22 +0100

    maby

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 
 import bubblesort as bs
+<<<<<<< HEAD
 import Mergesortv2 as mg
 import Quicksort as qs
 import insertionsort as io
 import streamlit as st
 
+=======
+#from bubblesort import bubble_sort
+
+#buble as class import
+#merge as class impoert
+#buttnes
+>>>>>>> parent of f4e5614 (all good)
 st.title("Sorteringsalgoritmer")
 
-result = st.button("Run all")
+result = st.button("bubblesort")
 
+<<<<<<< HEAD
 st.title("Bubblesort")
 
 st.write (bs.bubble_sort_tester)
@@ -16,11 +25,22 @@ st.write (bs.bubble_sort_tester)
 st.title("Mergesort")
 
 st.write (mg.merge_sort_tester)
+=======
+st.write(result)
 
-st.title("Quicksort")
+result = st.button("Merge sort")
 
+st.write(result)
+>>>>>>> parent of f4e5614 (all good)
+
+result = st.button("Quicksort")
+
+<<<<<<< HEAD
 st.write (qs.quick_sort_tester)
 
 st.title("Insertionsort")
 
 st.write (io.insertion_sort_tester)
+=======
+st.write(result)dd
+>>>>>>> parent of f4e5614 (all good)
