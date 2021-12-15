06a953e4b20694f5aa42b68c4e486319ca995b83
DunnerD <duncanmjk@gmail.com>
2021-12-16 00:10:25 +0100

    no

--- Quicksort.py
+++ Quicksort.py
@@ -46,9 +46,5 @@ def quick_sort(start, end, array):
 array = [ 10, 7, 8, 9, 1, 5 ]
 quick_sort(0, len(array) - 1, array)
 
-<<<<<<< HEAD
-print(quick_sort_tester(10000))
-=======
 print(f'Sorted array: {array}')
 	
->>>>>>> parent of f4e5614 (all good)
--- main.py
+++ main.py
@@ -1,46 +1,20 @@
 
 import bubblesort as bs
-<<<<<<< HEAD
 import Mergesortv2 as mg
 import Quicksort as qs
 import insertionsort as io
 import streamlit as st
 
-=======
-#from bubblesort import bubble_sort
-
-#buble as class import
-#merge as class impoert
-#buttnes
->>>>>>> parent of f4e5614 (all good)
 st.title("Sorteringsalgoritmer")
 
 result = st.button("bubblesort")
 
-<<<<<<< HEAD
-st.title("Bubblesort")
-
-st.write (bs.bubble_sort_tester)
-
-st.title("Mergesort")
-
-st.write (mg.merge_sort_tester)
-=======
 st.write(result)
 
 result = st.button("Merge sort")
 
 st.write(result)
->>>>>>> parent of f4e5614 (all good)
 
 result = st.button("Quicksort")
 
-<<<<<<< HEAD
-st.write (qs.quick_sort_tester)
-
-st.title("Insertionsort")
-
-st.write (io.insertion_sort_tester)
-=======
 st.write(result)dd
->>>>>>> parent of f4e5614 (all good)
